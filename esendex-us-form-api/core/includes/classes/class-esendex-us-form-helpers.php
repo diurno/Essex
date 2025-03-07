6193434a2f237facae4d30dee2c968b3708c9085
federico cadierno <diurnodiez@gmail.com>
2025-03-07 10:47:18 -0300

    unzipgit

---
+++ esendex-us-form-api/core/includes/classes/class-esendex-us-form-helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Class Esendex_Us_Form_Helpers
+ *
+ * This class contains repetitive functions that
+ * are used globally within the plugin.
+ *
+ * @package		ESENDEXUSF
+ * @subpackage	Classes/Esendex_Us_Form_Helpers
+ * @author		500designs
+ * @since		1.0.0
+ */
+class Esendex_Us_Form_Helpers{
+
+	/**
+	 * ######################
+	 * ###
+	 * #### CALLABLE FUNCTIONS
+	 * ###
+	 * ######################
+	 */
+
+}
